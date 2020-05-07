ec4675d25e22f8b48d538061d1fe9680091feb93
Casie Siekman <casie.siekman@gmail.com>
2020-05-07 11:01:41 -0500

    update pool

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,3 +1,13 @@
+/**
+* You'll need to use environment variables in order to deploy your
+* pg-pool configuration to Heroku.
+* It will look something like this:
+**/
+/* the only line you likely need to change is
+ database: 'prime_app',
+ change `prime_app` to the name of your database, and you should be all set!
+*/
+
 const pg = require('pg');
 const url = require('url');
 
@@ -15,22 +25,30 @@ if (process.env.DATABASE_URL) {
     host: params.hostname,
     port: params.port,
     database: params.pathname.split('/')[1],
-    ssl: true, 
-    max: 10,
-    idleTimeoutMillis: 30000, 
+    ssl: true, // heroku requires ssl to be true
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
 } else {
   config = {
-    host: 'localhost', 
-    port: 5432,
-    database: 'prime_app', 
-    max: 10, 
-    idleTimeoutMillis: 30000, 
+    host: 'localhost', // Server hosting the postgres database
+    port: 5432, // env var: PGPORT
+    database: process.env.DATABASE_NAME || 'prime_app', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
 }
 
+// this creates the pool that will be shared by all other modules
 const pool = new pg.Pool(config);
 
+// the pool will log when it connects to the database
+pool.on('connect', () => {
+  console.log('Postgesql connected');
+});
+
+// the pool with emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
 pool.on('error', (err) => {
   console.log('Unexpected error on idle client', err);
   process.exit(-1);
