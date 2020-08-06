3bcac867f2be649e004fb745b4f0c2393d190841
Casie Siekman <casie.siekman@gmail.com>
2020-08-06 15:34:55 -0500

    add heroku stuff

--- src/components/ImageUpload/ImageUpload.js
+++ src/components/ImageUpload/ImageUpload.js
@@ -6,7 +6,7 @@ import DropzoneS3Uploader from 'react-dropzone-s3-uploader';
 class ImageUpload extends Component {
     render(){
         const uploadOptions = {
-            server: 'http://localhost:5000',
+            server: 'https://glacial-retreat-32572.herokuapp.com/',
             // signingUrlQueryParams: {uploadType: 'avatar'},
         }
         const s3Url = 'https://yoga4now.s3.amazonaws.com'      
