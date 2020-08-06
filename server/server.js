aefc66834cc9b92c8699b395b343716ee2876138
Casie Siekman <casie.siekman@gmail.com>
2020-08-06 10:38:14 -0500

    connect to aws s3

--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ const passport = require('./strategies/user.strategy');
 const userRouter = require('./routes/user.router');
 const flowsRouter = require('./routes/flows.router');
 
+const UploaderS3Router = require('react-dropzone-s3-uploader/s3router');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -19,8 +21,17 @@ app.use(sessionMiddleware);
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use('/s3', UploaderS3Router({
+  bucket: 'yoga4now',                           // required
+  region: 'us-east-2',                            // optional
+  headers: {'Access-Control-Allow-Origin': '*'},  // optional
+  ACL: 'public-read',                                 // this is the default - set to `public-read` to let anyone view uploads
+}));
+
+
 app.use('/api/user', userRouter);
 app.use('/api/flows', flowsRouter);
+
 
 app.use(express.static('build'));
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Footer.css'
+import ImageUpload from '../ImageUpload/ImageUpload';
 
 const Footer = () => (
   <footer>
     &copy; Prime Digital Academy
+    <ImageUpload/>
   </footer>
 );
 
