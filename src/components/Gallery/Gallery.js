b9fe3e9f626c5554647fe5dd9dcb7308b780419b
Casie Siekman <casie.siekman@gmail.com>
2020-08-10 19:10:38 -0500

    add gallery and buttons

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import GalleryItem from '../GalleryItem/GalleryItem';
 
 import './Gallery.css';
 
 class Gallery extends Component {
 
+  state={
+    index: 0,
+  }
   componentDidMount () {
     const flowId = Number(this.props.location.search.slice(1,this.props.location.search.length));
     this.props.dispatch({type: 'FETCH_CHOSEN_FLOW', payload: flowId});
   }
 
+  onClick = (e, direction) => {
+    if(direction === "back"){
+      if(this.state.index === 0 ){
+        this.setState({
+          index: this.props.state.chosenFlow.length-1
+        })
+      } else {
+        this.setState({
+          index: this.state.index - 1
+        })
+      }
+    }
+
+    if(direction === "forward"){
+        if(this.state.index === this.props.state.chosenFlow.length-1){
+        this.setState({
+          index: 0
+        })
+      } else {
+        this.setState({
+          index: this.state.index + 1
+        })
+      }
+    }
+  }
+
   render() {
     return (
       <div>
-        {this.props.state.flows[0] ? 
-          <ul>{this.props.state.chosenFlow.map((item)=> {
-            return <li>
-              {item.name} 
-              <img className="image" src={item.imageUrl}/>
-            </li>
-          })}</ul>
+        {this.props.state.chosenFlow[0] ? 
+          <div className="galleryWrapper">
+            <button onClick={(e) => {this.onClick(e, 'back')}}> - </button>
+            <GalleryItem item={this.props.state.chosenFlow[this.state.index]}></GalleryItem>
+            <button onClick={(e) => {this.onClick(e, 'forward')}}> + </button>
+          </div>
           :
           <p></p>
         }
---
+++ src/components/GalleryItem/GalleryItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import './GalleryItem.css';
+
+class GalleryItem extends Component {
+
+  state={
+    index: 0,
+  }
+
+  render() {
+    return (
+      <div>
+        <img className="image" src={this.props.item.imageUrl}/>
+        <p>{this.props.item.name}</p>
+      </div>
+    )
+  }
+}
+
+
+const mapStateToProps = state => ({
+  state
+});
+
+export default connect(mapStateToProps)(GalleryItem);
